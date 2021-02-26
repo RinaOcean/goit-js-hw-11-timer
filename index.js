4216bc0defe79362dcc8f350b639ec5ac4c21940
RinaOcean <antyfifa@gmail.com>
2021-02-27 01:26:19 +0200

    timer done

---
+++ index.js
@@ -0,0 +1,31 @@
+const targetTime = new Date(2022, 0, 1, 0, 0, 0);
+
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+setInterval(() => {
+  const curentTime = new Date();
+  const time = targetTime - curentTime;
+  updateTimer(time);
+}, 1000);
+
+function updateTimer(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
